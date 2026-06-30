60f8cf92231a34133646737d8f4dd0f4ea1d6129
Akshat Choudhary <akshatch2131@gmail.com>
2026-06-30 19:28:35 +0530

    Playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,32 +2,42 @@ const express=require("express")
 const app=express()
 
 
-app.use("/user",(req,res)=>{
-    res.send("HAHAHAHA")
-})
+// app.use("/user",(req,res)=>{
+//     res.send("HAHAHAHA")
+// })
 
 
 //this will only handle GET call to /user
+// app.get("/user",(req,res)=>{
+//     res.send({firstname: "Akshat",lastName: "Chaudhary"})
+// })
+
+
+//If we replace abc by ab?c then ac is also work
+//If we replace abc by ab+c then abbbbbbbbbbc is also work
+//If we replace abc by ab*cd then abAKSHATcd is also work
+//If we replace abc by a(bc)?d then bc is optional
+//If we replace abc by /a/ if path is contains any a in between then it works
 app.get("/user",(req,res)=>{
     res.send({firstname: "Akshat",lastName: "Chaudhary"})
 })
 
 
-app.post("/user",(req,res)=>{
-    console.log("Saves the data into the database");
-    res.send("Data sucessfully saved to the database");
-})
+// app.post("/user",(req,res)=>{
+//     console.log("Saves the data into the database");
+//     res.send("Data sucessfully saved to the database");
+// })
 
 
-app.delete("/user",(req,res)=>{
-    res.send("Deleted Succesfully ");
-})
+// app.delete("/user",(req,res)=>{
+//     res.send("Deleted Succesfully ");
+// })
 
 
-//this use will match all the HTTP method API calls to /test
-app.use("/test",(req,res)=>{  //roiute handler
-    res.send("Hello from the new server")
-})  //Any other routes after this written does not work
+// //this use will match all the HTTP method API calls to /test
+// app.use("/test",(req,res)=>{  //roiute handler
+//     res.send("Hello from the new server")
+// })  //Any other routes after this written does not work
 
 app.listen(3000,()=>{ 
     console.log("Server is successfully running");
